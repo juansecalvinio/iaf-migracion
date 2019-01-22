40ddedc57e6b9d927fada7ada7889e657a5d67b1
Juanse <j.s.calvinio@gmail.com>
2019-01-22 11:12:50 -0300

    Se agrega paquete 'cron' para schedulearlo como servicio

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const loggerFile  = require('./controladores/logger').getLogger('file');
+const CronJob = require('cron').CronJob;
 
 const app = express();
 
@@ -15,14 +16,23 @@ app.use('/', migracion);
 const puerto = '3335';
 
 const httpRequest = () => {
-    loggerFile.info('Realizando HTTP Request');
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             request({ url: 'http://localhost:3335/', json: true }, (error, response) => {
                 if(error) {
                     request({ url: 'http://localhost:3335/', json: true }, (error, response) => {
                         if(error) {
-                            reject(error);
+                            request({ url: 'http://localhost:3335/', json: true }, (error, response) => {
+                                if(error) {
+                                    reject(error);
+                                } else if (response.statusCode == 404) {
+                                    reject(response.body);
+                                } else if (response.statusCode == 'ECONNRESET') {
+                                    reject(response.body);
+                                } else {
+                                    resolve(response.body);
+                                }
+                            })
                         } else if (response.statusCode == 404) {
                             reject(response.body);
                         } else if (response.statusCode == 'ECONNRESET') {
@@ -31,7 +41,6 @@ const httpRequest = () => {
                             resolve(response.body);
                         }
                     });
-                    reject(error);
                 } else if (response.statusCode == 404) {
                     reject(response.body);
                 } else if (response.statusCode == 'ECONNRESET') {
@@ -44,10 +53,17 @@ const httpRequest = () => {
     });
 }
 
-httpRequest().then()
-.catch(err => {
-    loggerFile.error(err);
+const job = new CronJob('00 30 05 * * *', () => {
+    httpRequest().then(() => {
+        loggerFile.info('Realizando HTTP Request.');
+    })
+    .catch(err => {
+        loggerFile.error(err);
+    })
 });
+
+job.start();
+
 
 app.listen(puerto, () => {
     loggerFile.info(`Escuchando desde el puerto ${puerto}`);
